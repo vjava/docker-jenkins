b2519c6168e004c9954703ebab54ca7f63de99d5
VJAVA <vidya4java@gmail.com>
2020-01-04 13:33:20 +0530

    First Commit

---
+++ src/main/java/com/java/SpringDockerJenkinsApplication.java
@@ -0,0 +1,13 @@
+package com.java;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringDockerJenkinsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringDockerJenkinsApplication.class, args);
+	}
+
+}
